1e1719161106cd94f669c47dc8892a14a474c917
alexcl11 <aleecanovaslopez@gmail.com>
2026-02-06 13:09:27 +0100

    up

---
+++ MvcSegundaPracticaACL/Controllers/ComicsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcSegundaPracticaACL.Models;
+using MvcSegundaPracticaACL.Repositories;
+
+namespace MvcSegundaPracticaACL.Controllers
+{
+    public class ComicsController : Controller
+    {
+        RepositoryComics repo;
+
+        public ComicsController()
+        {
+            this.repo = new RepositoryComics();
+
+        }
+        public IActionResult Index()
+        {
+            List<Comic> comic = this.repo.GetComics();
+            return View(comic);
+        }
+
+        public IActionResult Detalles(int id)
+        {
+            Comic comic = this.repo.DetallesComic(id);
+            return View(comic);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string nombre, string img, string descripcion)
+        {
+            await this.repo.CreateComicAsync(nombre, img, descripcion);
+            return RedirectToAction("Index");
+        } 
+    }
+}
---
+++ MvcSegundaPracticaACL/Repositories/RepositoryComics.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Data.SqlClient;
+using MvcSegundaPracticaACL.Models;
+using System.Data;
+
+namespace MvcSegundaPracticaACL.Repositories
+{
+    public class RepositoryComics
+    {
+        DataTable tablaComics;
+        SqlConnection cn;
+        SqlCommand com;
+        public RepositoryComics()
+        {
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=COMICS;User ID=sa;Trust Server Certificate=True";
+            string sql = "select * from COMICS";
+            SqlDataAdapter ad = new SqlDataAdapter(sql, connectionString);
+            this.tablaComics = new DataTable();
+            ad.Fill(this.tablaComics);
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+        public List<Comic> GetComics()
+        {
+            var consulta = from datos in this.tablaComics.AsEnumerable()
+                           select datos;
+
+            List<Comic> comics = new List<Comic>();
+
+            foreach (var row in consulta)
+            {
+                Comic comic = new Comic();
+                comic.IdComic = row.Field<int>("IDCOMIC");
+                comic.Nombre = row.Field<string>("NOMBRE");
+                comic.Imagen = row.Field<string>("IMAGEN");
+                comic.Descripcion = row.Field<string>("DESCRIPCION");
+                comics.Add(comic);
+            }
+            return comics;
+        }
+
+        public Comic DetallesComic(int id)
+        {
+            var consulta = from datos in this.tablaComics.AsEnumerable()
+                           where datos.Field<int>("IDCOMIC") == id
+                           select datos;
+
+            var row = consulta.First();
+            Comic comic = new Comic();
+            comic.IdComic = row.Field<int>("IDCOMIC");
+            comic.Nombre = row.Field<string>("NOMBRE");
+            comic.Imagen = row.Field<string>("IMAGEN");
+            comic.Descripcion = row.Field<string>("DESCRIPCION");
+            return comic;
+        }
+
+        public async Task CreateComicAsync(string nombre, string img, string descripcion)
+        {
+            var consulta = from datos in this.tablaComics.AsEnumerable()
+                           select datos;
+            int maxID = consulta.Max(x => x.Field<int>("IDCOMIC"));
+
+            string sql = "INSERT INTO COMICS VALUES(@id, @nombre, @imagen, @descripcion)";
+            this.com.Parameters.AddWithValue("@id", maxID+1);
+            this.com.Parameters.AddWithValue("@nombre", nombre);
+            this.com.Parameters.AddWithValue("@imagen", img);
+            this.com.Parameters.AddWithValue("@descripcion", descripcion);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            await this.com.ExecuteNonQueryAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+        }
+    }
+}
